2ded8167368ec2a70aebd5c58733b7a179a67fcb
MixSrb <miloradmarkov@gmail.com>
2020-11-23 17:17:27 +0000

    Initial commit

---
+++ progkod22_2.cpp
@@ -0,0 +1,16 @@
+#include<algorithm>
+#include<vector>
+#include<iostream>
+using namespace std;
+int main();
+{
+   vector<int> numsInVec{ 25, 101, 2017, -50 };
+
+   auto evenNum = find_if(numsInVec.cbegin(),
+                          numsInVec.cend(),  // range to find in
+                  [] (const int& num) {return ((num % 2) == 0);} );
+   if  (eveNum != numsInVec.cend())
+       cout << "Even number in collection is: " << *evenNum << endl;
+
+   return 0:
+}
